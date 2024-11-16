c760e65245ca7d0b0fa8adce998a778deecac1b3
Maciej <m.kulinski@codelabs.pl>
2024-11-16 18:11:24 +0100

    Updates

--- src/events/order/order-created-event.ts
+++ src/events/order/order-created-event.ts
@@ -2,7 +2,7 @@ import { Subjects } from "../subjects";
 import { OrderStatus } from "../types/order-status";
 
 export type OrderCreatedEvent = {
-  subjects: Subjects.OrderCreated;
+  subject: Subjects.OrderCreated;
   data: {
     id: string;
     status: OrderStatus;
