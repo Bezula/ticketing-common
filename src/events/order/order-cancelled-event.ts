2d59841542bebc2365d53591d2dfd67abdf49ebd
Maciej <m.kulinski@codelabs.pl>
2024-11-16 19:14:00 +0100

    Updates

--- src/events/order/order-cancelled-event.ts
+++ src/events/order/order-cancelled-event.ts
@@ -4,6 +4,7 @@ export type OrderCancelledEvent = {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
+    version: number;
     ticket: {
       id: string;
     };
--- src/events/ticket/ticket-updated-event.ts
+++ src/events/ticket/ticket-updated-event.ts
@@ -4,6 +4,7 @@ export type TicketUpdatedEvent = {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
+    version: number;
     title: string;
     price: number;
     userId: string;
